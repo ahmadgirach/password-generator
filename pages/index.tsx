8787a3de7f7db19b0e47cab0b58dac167ef56bc7
Ahmad Girach <ahmedkhatri6@gmail.com>
2022-06-15 13:07:02 +0530

    [CHANGE/ADD] fonts to public sans and added toast notification

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import React, { useEffect, useState } from 'react'
 import { generatePassword } from '../utils'
+import { Toaster, toast } from 'react-hot-toast'
 
 const Home: NextPage = () => {
 
@@ -17,7 +18,8 @@ const Home: NextPage = () => {
   }
 
   const handleUppercaseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUppercase(e.target.checked)
+    // DON'T ALLOW TO UNCHECK IT.
+    e.preventDefault()
   }
 
   const handleLowercaseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,7 +41,9 @@ const Home: NextPage = () => {
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(password)
-    alert("Password has been copied to clipboard")
+    return toast('Copied to Clipboard!', {
+      duration: 1000,
+    })
   }
 
   useEffect(() => {
@@ -47,54 +51,59 @@ const Home: NextPage = () => {
   }, [uppercase, lowercase, numbers, symbols, length])
 
   return (
-    <div className="flex flex-col items-center justify-center py-2 min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen font-primary">
       <Head>
         <title>Password Manager</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center bg-sky-100">
-        <section className="w-full rounded-md shadow-md p-4 bg-white space-y-4">
+        <section className="w-3/4 rounded-md shadow-md bg-white flex flex-col justify-center items-center gap-4 p-10">
           <h2 className="font-bold text-3xl text-sky-600 uppercase">Password Generator Tool</h2>
-          <h1 className="font-bold text-xl">Use my online password generator tool to create a secure unique password and stay safe online.</h1>
+          <h1 className="text-xl -mt-3">Generate a secure and unique password effortlessly.</h1>
 
-          <div className="w-full bg-white p-2 strength-indicator"></div>
-
-          <div className='flex justify-center items-center gap-4'>
-            <textarea name="password" id="password" cols={20} rows={3} value={password} className='bg-gray-200 p-4 outline-none w-3/4 rounded-md text-center min-w-fit' readOnly></textarea>
+          <div className='flex flex-col md:flex-row justify-center items-center gap-4 w-full'>
+            <textarea name="password" id="password" cols={20} rows={2} value={password} className='bg-sky-50 outline-none w-3/4 rounded-md text-center text-xl text-black tracking-wide' readOnly></textarea>
             <button title='Copy to Clipboard' onClick={copyToClipboard}>
-              <svg xmlns="http://www.w3.org/2000/svg" className='h-10 text-sky-800' fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+              <svg xmlns="http://www.w3.org/2000/svg" className='h-7 text-sky-800' fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
               </svg>
             </button>
             <button title='Regenerate Password' onClick={regeneratePassword}>
-              <svg xmlns="http://www.w3.org/2000/svg" className='h-9 text-sky-800' fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <svg xmlns="http://www.w3.org/2000/svg" className='h-7 text-sky-800' fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
               </svg>
             </button>
           </div>
 
-          <div className='flex justify-center items-center gap-4'>
-            <div className='flex flex-col'>
-              <p>Password Length</p>
+          <div className='flex flex-col md:flex-row justify-center items-center gap-x-40'>
+            <div className='space-y-2 sm:mt-6'>
+              <p className='text-lg'>Password Length</p>
               <input type="range" name="length" id="length" value={length} onChange={handleSliderChange} />
+              <p>{length}</p>
             </div>
-            <div className='flex flex-col'>
-              <div className='space-x-2'>
-                <input type="checkbox" name="uppercase" id="uppercase" checked={uppercase} onChange={handleUppercaseChange} />
-                <label htmlFor="uppercase">Uppercase</label>
+            <div className='text-left space-y-3'>
+              <div className='flex items-center gap-2 sm:mt-5'>
+                <input
+                  type="checkbox"
+                  name="uppercase"
+                  id="uppercase"
+                  checked={uppercase}
+                  onChange={handleUppercaseChange}
+                />
+                <label htmlFor="uppercase" className='cursor-pointer'>Uppercase</label>
               </div>
-              <div className='space-x-2'>
+              <div className='flex items-center gap-2'>
                 <input type="checkbox" name="lowercase" id="lowercase" checked={lowercase} onChange={handleLowercaseChange} />
-                <label htmlFor="lowercase">Lowercase</label>
+                <label htmlFor="lowercase" className='cursor-pointer'>Lowercase</label>
               </div>
-              <div className='space-x-2'>
+              <div className='flex items-center gap-2'>
                 <input type="checkbox" name="numbers" id="numbers" checked={numbers} onChange={handleNumbersChange} />
-                <label htmlFor="numbers">Numbers</label>
+                <label htmlFor="numbers" className='cursor-pointer'>Numbers</label>
               </div>
-              <div className='space-x-2'>
+              <div className='flex items-center gap-2'>
                 <input type="checkbox" name="symbols" id="symbols" checked={symbols} onChange={handleSymbolsChange} />
-                <label htmlFor="symbols">Symbols</label>
+                <label htmlFor="symbols" className='cursor-pointer'>Symbols</label>
               </div>
             </div>
           </div>
@@ -113,6 +122,8 @@ const Home: NextPage = () => {
           &nbsp;Ahmad Girach
         </a>
       </footer>
+
+      <Toaster />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Public Sans', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+};
