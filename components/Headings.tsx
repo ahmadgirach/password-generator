690b9298a584971cc731e5ef096bd00ba04f21eb
Ahmad Girach <ahmedkhatri6@gmail.com>
2022-06-25 20:41:05 +0530

    [IMP] headings margin

--- components/Headings.tsx
+++ components/Headings.tsx
@@ -1,10 +1,8 @@
 export const Headings = () => {
   return (
     <>
-      <h2 className="font-bold text-3xl uppercase mt-10">Password Generator</h2>
-      <h1 className="text-xl">
-        Generate a strong password effortlessly.
-      </h1>
+      <h2 className="font-bold text-3xl uppercase mt-3">Password Generator</h2>
+      <h1 className="text-xl">Generate a strong password effortlessly.</h1>
     </>
   );
 };
